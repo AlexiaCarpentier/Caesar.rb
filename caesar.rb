03dad3cd85af40bd0845e39804d78c8b5e188316
AlexiaCarpentier <33696549+AlexiaCarpentier@users.noreply.github.com>
2017-11-16 20:15:55 +0100

    Update caesar.rb

--- caesar.rb
+++ caesar.rb
@@ -1,18 +1,7 @@
-def chiffre_de_cesar(string, shift = 5)
-  alphabet  = Array('a'..'z')
-  non_caps  = Hash[alphabet.zip(alphabet.rotate(shift))]
-  
-  alphabet = Array('A'..'Z')
-  caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
+test = gets.chomp
 
-  alphabet = Array('!'..'?')
-  symbol     = Hash[alphabet.zip(alphabet.rotate(shift))]
+ascii = test.chars.map { |c| c.ord }
+shifted = ascii.map { |c| c + 5 }
+caesar_cipher = shifted.map { |c| c.chr }.join
 
-   encrypter = symbol.merge(non_caps.merge(caps))
- 
-  string.chars.map { |c| encrypter.fetch(c, c) }
-end
-
-text = gets.chomp
-
-p chiffre_de_cesar("Alexia et Sebatien!").join
+puts caesar_cipher
